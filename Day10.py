3648943b9ee0a16a2fcc7289b72fdbc6aa0cf022
mricardo <miguelcricardo@gmail.com>
2017-12-14 22:31:32 +0000

    Day 14

--- Day10.py
+++ Day10.py
@@ -1,11 +1,7 @@
 from operator import xor
 from functools import reduce
 
-h = [x for x in range(0, 256)]
-pos = 0
-skip_size = 0
-
-def reverse_h(length, pos):
+def reverse_h(h, length, pos):
    i = 0
    size = len(h)
 
@@ -19,34 +15,37 @@ def reverse_h(length, pos):
        
        i += 1  
 
-def do_hash(lengths):
-    global pos
-    global skip_size
-
+def do_hash(h, lengths, pos, skip_size, ):
     for l in lengths:  
-        reverse_h(l, pos)
+        reverse_h(h, l, pos)
         pos += (l + skip_size)
         skip_size += 1
         
-    return h[0] * h[1]
+    return (pos, skip_size)
 
-def dense_hash():
+def hash(h, input):
+        pos = 0
+        skip_size = 0
+
+        lengths = []
+        lengths = [ord(l) for l in input]
+        lengths.extend([17, 31, 73, 47, 23])
+        for i in range(0, 64):
+            (pos, skip_size) = do_hash(h, lengths, pos, skip_size) 
+
+def knot_hash(input):
+    h = [x for x in range(0, 256)]
+
+    hash(h, input)
     dense = list()
  
     for i in range (0, 16):        
         dense.append(reduce(xor, h[i*16:(i+1)*16]))
     
-    print(''.join((format(x, '02x') for x in dense)))
+    return ''.join((format(x, '02x') for x in dense))
 
-def hash():
+def read():
     with open('input') as f:
-        lengths = []
-        for line in f:
-          lengths = [ord(l) for l in line]
+        return f.readline()
 
-        lengths.extend([17, 31, 73, 47, 23])
-        for i in range(0, 64):
-            do_hash(lengths) 
-
-hash()
-dense_hash()
+#knot_hash(read())
---
+++ Day14.py
@@ -0,0 +1,49 @@
+import time
+from Day10 import knot_hash
+
+def to_binary(input):
+    b = []      
+    for l in input:
+        b.extend([int(x) for x in bin(int(l, 16))[2:].zfill(4)])
+    
+    return b
+
+def floodfill(disk, x, y, group):    
+    if disk[x][y] == 1:  
+        disk[x][y] = group        
+    
+        if x > 0:
+            floodfill(disk, x - 1, y, group)
+
+        if x < len(disk[y]) - 1:
+            floodfill(disk, x + 1, y, group)
+
+        if y > 0:
+            floodfill(disk, x, y - 1, group)
+
+        if y < len(disk) - 1:
+            floodfill(disk, x, y + 1, group)
+    
+def calculate_region(disk, group, i, j):
+    group = 1
+    for x, row in enumerate(disk):
+        for y, cell in enumerate(row):
+            if (cell == 1):                 
+                group += 1
+                floodfill(disk, x, y, group)                 
+
+    return group - 1
+
+def calculate_fills_and_region(key):
+    fills = 0
+    disk = []
+    for i in range(0, 128):
+        b = to_binary(knot_hash(key + str(i)))
+        disk.append(b)
+        fills += b.count(1)
+
+    print(fills)    
+    print(calculate_region(disk, 0, 0, 0))
+
+calculate_fills_and_region("nbysizxe-")
+# 128 knot hash
